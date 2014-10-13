d19b78e2769c204aa0c552e77baaa5c090b5b92e
syedfasih <syedfasih@gmail.com>
2014-10-13 16:19:02 +0500

    Test API

---
+++ TestApi/Controllers/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace TestApi.Controllers
+{
+    [DataContract(Namespace = "", Name = "Customer")]
+    
+    public class Customer
+    {
+        [DataMember(Name = "CustomerId")]
+        public int CustomerId { get; set; }
+
+        [DataMember(Name = "CustomerType")]
+        public CustomerTypeDTO CustomerTypeDto { get; set; }
+
+        [DataMember(Name = "Name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "Orders")]
+        public List<OrderDTO> Orders { get; set; }
+    }
+
+    [DataContract(Namespace = "", Name = "CustomerType")]
+    public class CustomerTypeDTO
+    {
+        [DataMember(Name = "CustomerTypeId")]
+        public int CustomerTypeId { get; set; }
+
+        [DataMember(Name = "TypeCode")]
+        public string TypeCode { get; set; }
+
+    }
+
+    [DataContract(Namespace = "", Name = "Order")]
+    public class OrderDTO
+    {
+        [DataMember(Name = "OrderId")]
+        public int OrderId { get; set; }
+
+        [DataMember(Name = "CustomerId")]
+        public int CustomerId { get; set; }
+
+        [DataMember(Name = "Amount")]
+        public decimal Amount { get; set; }
+    }
+
+}
+
+
---
+++ TestApi/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TestApi.Controllers
+{
+    [RoutePrefix("customers")]
+    public class CustomerController : ApiController
+    {
+        [Route("putcustomer")]
+        public async Task<HttpResponseMessage> Put(List<Customer> customers)
+        {
+            //var orders = customers[0].Orders;
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
